49c77c415edfa4436c35527fe16a6f4edaf955c5
bhavyejain <bhavyej@gmail.com>
2023-02-09 21:45:57 -0800

    increase buffer size

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CLIENT_PORTS = {"A": 9260, "B": 9261, "C": 9262, "D": 9263, "E": 9264}
 CONNECTIONS = {"A" : {"B"}, "B" : {"A", "D"}, "C" : {"B"}, "D" : {"A", "B", "C", "E"}, "E" : {"B", "D"}}
-BUFF_SIZE = 1024
+BUFF_SIZE = 2048
 HOST = '127.0.0.1'
 DEF_DELAY = 3.00
 TOKEN_HOLD_TIME = 1.00
